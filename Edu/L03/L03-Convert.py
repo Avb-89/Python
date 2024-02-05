bf89b7a2bd9e31772a758763dfdfec1284545981
Alexey Vadimovich <avb-89@outlook.com>
2024-02-05 21:34:44 +0300

    new

---
+++ Edu/L03/L03-Convert.py
@@ -0,0 +1,22 @@
+num = input(
+    'What you want? \n '
+    '\n'
+    '1) Convert Byte \n'
+    '2) Convert Megabyte \n'
+    '3) Convert Gigabyte \n'
+    '4) Convert TerraByte \n'
+)
+# TODO: воспользоваться блоками кода иначе получается не меню а херь. Дальше дерьмо, т.к не сработает меню
+if num = (1)
+
+numByte = input(
+    '1) Convert Byte to Megabyte \n'
+    '2) Convert Byte to Gigabyte \n'
+    '3) Convert Byte to TerraByte \n'
+)
+
+numMb = input(
+    '1) Convert Megabyte to Byte \n'
+    '2) Convert Megabyte to Gigabyte \n'
+    '3) Convert Megabyte to TerraByte \n'
+)
