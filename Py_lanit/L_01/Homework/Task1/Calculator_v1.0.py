10b815151542a823ff022918c9d92f97226db472
Alexey Vadimovich <avb-89@outlook.com>
2024-02-06 03:02:51 +0300

    Calc_v1.0

--- Py_lanit/L_01/Homework/Task1/Calculator_v1.0.py
+++ Py_lanit/L_01/Homework/Task1/Calculator_v1.0.py
@@ -1,3 +1,16 @@
-print('Калькулятор v1.0 который умеет работать с целыми числами \n Ваc вас приветствует программа калькулятор. \n ')
+print('Калькулятор v1.0 который умеет работать с целыми числами \n Ваc приветствует программа калькулятор. \n ')
 import vars
+if vars.choise == '1':
+    import summ
+elif vars.choise == '2':
+    import sub
+elif vars.choise == '3':
+    import myltiply
+elif vars.choise == '4':
+    import div
+else:
+    print("Такого пункта в списке, к сожалению, нет")
+
+
+
 
--- Py_lanit/L_01/Homework/Task1/vars.py
+++ Py_lanit/L_01/Homework/Task1/vars.py
@@ -1,4 +1,6 @@
-choise = input('я хочу (выберите пункт):\n 1) Сложить \n 2) Вычесть \n 3) Умножить \n 4) Разделить \n')
+choise = input('Я хочу (выберите пункт):\n 1) Сложить \n 2) Вычесть \n 3) Умножить \n 4) Разделить \n 5) Выйти \n ')
+if choise == '5':
+    exit()
 while not choise.isdigit():
     print('Выберите пункт и введите его номер (без скобочки)')
     choise = input('')
@@ -6,7 +8,7 @@ chislo_1 = input("Введите, пожалуйста первое число \
 while not chislo_1.isdigit():
     print('Укажите первое число в цифровом формате')
     chislo_1 = input('')
-chislo_2 = input("введите, пожалуйста второе число \n")
+chislo_2 = input("Введите, пожалуйста второе число \n")
 while not chislo_2.isdigit():
     print('Укажите второе число в цифровом формате')
     chislo_2 = input('')
@@ -14,3 +16,4 @@ summ=int(chislo_1) + int(chislo_2)
 sub=int(chislo_1) - int(chislo_2)
 multiply=int(chislo_1) * int(chislo_2)
 div=int(chislo_1) / int(chislo_2)
+cycle = 1
