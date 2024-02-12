4bbf7f636f0a26e0f10f74b50b2f7c3aa8d378f4
Alexey Vadimovich <avb-89@outlook.com>
2024-02-12 15:22:43 +0300

    вернул как было

--- Py_lanit/L_01/Homework/Task1/Calculator_v1.0.py
+++ Py_lanit/L_01/Homework/Task1/Calculator_v1.0.py
@@ -1,19 +1,16 @@
-from imp import reload
-
 print('Калькулятор v1.0 который умеет работать с целыми числами \n Ваc приветствует программа калькулятор. \n ')
-while(True):
-    import vars
-    reload(vars)
-    if vars.choise == '1':
-        import summ
-    elif vars.choise == '2':
-        import sub
-    elif vars.choise == '3':
-        import myltiply
-    elif vars.choise == '4':
-        import div
-    else:
-        print("Такого пункта в списке, к сожалению, нет")
+import vars
+
+if vars.choise == '1':
+    import summ
+elif vars.choise == '2':
+    import sub
+elif vars.choise == '3':
+    import myltiply
+elif vars.choise == '4':
+    import div
+else:
+    print("Такого пункта в списке, к сожалению, нет")
 
 
 
