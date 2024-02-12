1afb3f5af5a4ede6aaa4e7c3b45ab01bf9f5e4b0
Alexey Vadimovich <avb-89@outlook.com>
2024-02-12 15:50:25 +0300

    вернул как было

--- Py_lanit/L_01/Homework/Task1/Calculator_v1.0.py
+++ Py_lanit/L_01/Homework/Task1/Calculator_v1.0.py
@@ -1,6 +1,5 @@
 print('Калькулятор v1.0 который умеет работать с целыми числами \n Ваc приветствует программа калькулятор. \n ')
 import vars
-
 if vars.choise == '1':
     import summ
 elif vars.choise == '2':
@@ -11,8 +10,3 @@ elif vars.choise == '4':
     import div
 else:
     print("Такого пункта в списке, к сожалению, нет")
-
-
-
-
-
